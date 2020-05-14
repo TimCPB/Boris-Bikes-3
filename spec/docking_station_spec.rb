2f79746fed7b29cb0a2589491ef028ac3c61bc73
Student <student@Makerss-MacBook-Air.local>
2020-05-14 14:01:16 +0100

    changed docking_station_spec back to syntax I can uderstand more clearly

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,6 +5,6 @@ describe DockingStation do
     it {is_expected.to respond_to(:release_bike)}
     it 'release_bike' do
         bike = subject.release_bike
-        expect(bike).to be_working
+        expect(bike.working?).to eq true
     end
 end
