7b487cea8e70865942c8fd1c8d6081f6ac4f53ec
Student <student@Makerss-MacBook-Air.local>
2020-05-15 01:11:42 +0100

    Tidied up docking_station_spec.rb a tiny bit

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,25 +5,26 @@ describe DockingStation do
     subject(:station) {DockingStation.new}
 
     it {is_expected.to respond_to(:release_bike)}
+    
     it 'release_bike' do
         bike = station.release_bike
         expect(bike.working?).to eq true
     end
 
-  it {is_expected.to respond_to(:dock).with(1).argument }
+    it {is_expected.to respond_to(:dock).with(1).argument }
 
 
     it { is_expected.to respond_to(:bike) }
 
-   it 'docks a bike' do
-     bike = Bike.new
-     expect(station.dock(bike)).to eq bike
-   end
+    it 'docks a bike' do
+        bike = Bike.new
+        expect(station.dock(bike)).to eq bike
+    end
 
-   it 'contains a bike' do
+    it 'contains a bike' do
         bike = Bike.new
         station.dock(bike)
         expect(station.bike).to eq bike
-      end
+    end
 
 end
